87af2af58269bafb9c3d20e4e45d7b8eabab57ca
choijinhap <wlsgkq123@gmail.com>
2019-12-13 09:43:32 +0900

    master test

--- MyCrawler/src/HelloGit.java
+++ MyCrawler/src/HelloGit.java
@@ -3,5 +3,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello git");
 		System.out.println("hello branch com");
+		System.out.println("master test");
 	}
 }
