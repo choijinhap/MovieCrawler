f5bdd07b84c4701637e485a5dfadb604c000a0bd
choijinhap <wlsgkq123@gmail.com>
2019-12-13 10:12:20 +0900

    변수 설명 추가

--- MyCrawler/src/MovieCrawler/MainApp.java
+++ MyCrawler/src/MovieCrawler/MainApp.java
@@ -6,14 +6,23 @@ public class MainApp {
 
 		NaverCrawler naver = new NaverCrawler(100);
 		Yes24Crawler yes24 = new Yes24Crawler(100);
-
+		/* 
+		 * String[] title; 
+		 * String[] price; 
+		 * 
+		 * NaverCrawler만 가지고 있음
+		 * String[] imgURI; 
+		 * String[] linkURL; 
+		 * String[] content; 
+		 * String[] type;
+		 */
 		naver.crawl();
 		yes24.crawl();
 
 		for (int i = 0; i < 100; i++) {
-			System.out.println(i+"번");
-			System.out.println(naver.getTitle()[i] +" "+ naver.getPrice()[i]);
-			System.out.println(yes24.getTitle()[i] +" "+ yes24.getPrice()[i]);
+			System.out.println(i + "번");
+			System.out.println(naver.getTitle()[i] + " " + naver.getPrice()[i]);
+			System.out.println(yes24.getTitle()[i] + " " + yes24.getPrice()[i]);
 			System.out.println("======");
 		}
 	}
